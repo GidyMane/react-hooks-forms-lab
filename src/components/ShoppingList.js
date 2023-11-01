d96c8cff384fa11fa440418a1e30e4356bf0fbd6
GidyMane <muragegideon2000@gmail.com>
2023-11-01 03:17:31 +0300

    complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,25 +5,54 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [myData , setMyData]=useState(items)
+  const [searchInput, setSearchInput]= useState("")
+  let filteredItem;
+  const [filteredData, setFilteredData]= useState()
+
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function updateData(data){
+    setMyData([...myData, data])
+  }
+  
+
+  let itemsToDisplay = myData.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  function updateFilter(e){
+    let value=e.target.value
+    setFilteredData([])
+    setSearchInput("")
+    setSearchInput(value)
+    console.log(searchInput)
+    let myFilteredData = myData.filter((item) => {
+      return item.name.toLowerCase().includes(searchInput);
+    });
+    setFilteredData(myFilteredData)
+  
+  }
+  if(filteredData != null){
+    filteredItem=filteredData.map((item) => {
+      return <Item key={item.id} name={item.name} category={item.category} />
+    })
+    
+}
+ 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={updateData}/>
+      <Filter onCategoryChange={handleCategoryChange} search={searchInput} onSearchChange={updateFilter} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        { filteredData != null? (filteredItem): (itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        )))}
       </ul>
     </div>
   );
